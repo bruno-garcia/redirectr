6a8882865e09c7960c32ef79edf7514b13244859
Bruno Garcia <bruno@brunogarcia.com>
2020-01-05 21:43:01 +0100

    aspnetcore package

--- src/Redirectr.AspNetCore/ServiceCollectionExtensions.cs
+++ src/Redirectr.AspNetCore/ServiceCollectionExtensions.cs
@@ -3,17 +3,18 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text.RegularExpressions;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Redirectr;
 
-namespace Redirectr
+// ReSharper disable once CheckNamespace -- Discoverability
+namespace Microsoft.Extensions.DependencyInjection
 {
     [EditorBrowsable(EditorBrowsableState.Never)]
     public static class ServiceCollectionExtensions
     {
         public static IServiceCollection AddRedirectr(this IServiceCollection services)
         {
-            services.TryAddSingleton<KeyGenerator>();
+            services.TryAddSingleton<IKeyGenerator, KeyGenerator>();
             services.TryAddSingleton<IRedirectrStore, InMemoryRedirectrStore>();
 
             services
@@ -21,6 +22,8 @@ namespace Redirectr
                 .Configure<IConfiguration>((o, c) => c.Bind("Redirectr", o))
                 .PostConfigure((RedirectrOptions o, IConfiguration c) =>
                 {
+                    o.Normalize();
+
                     if (o.BaseAddress is null)
                     {
                         o.BaseAddress = new Uri(
@@ -30,32 +33,6 @@ namespace Redirectr
                             ?? "http://localhost",
                             UriKind.Absolute); // With TestServer 'URLS' isn't defined
                     }
-
-                    // Normalize URLs. Blindly appending a forward slash to a Uri instance results in double slash
-                    // when that instance already terminated with a slash. To simplify the code further,
-                    // Options is expected to include a leading slash to all its URLs:
-                    var shortenUrlPath = o.ShortenUrlPath.ToString();
-                    if (!shortenUrlPath.EndsWith("/", StringComparison.Ordinal))
-                    {
-                        shortenUrlPath += "/";
-                        o.ShortenUrlPath = new Uri(shortenUrlPath, UriKind.Relative);
-                    }
-
-                    var shortUrlPath = o.ShortUrlPath?.ToString();
-                    if (shortUrlPath?.EndsWith("/", StringComparison.Ordinal) != true)
-                    {
-                        shortUrlPath += "/";
-                        o.ShortUrlPath = new Uri(shortUrlPath, UriKind.Relative);
-                    }
-
-                    // baseAddress now is expected to only be concat to a key to result in a final shortened URL.
-                    var baseAddress = o.BaseAddress.AbsoluteUri;
-                    if (!baseAddress.EndsWith("/", StringComparison.Ordinal) && shortUrlPath?[0] != '/')
-                    {
-                        baseAddress += "/";
-                        o.BaseAddress = new Uri(baseAddress, UriKind.Absolute);
-                    }
-
                 })
                 .Validate(o =>
                 {
@@ -68,8 +45,10 @@ namespace Redirectr
                     {
                         return false;
                     }
-                }, "Custom validation failed for members: 'RegexUrlCharacterWhiteList' with the error: 'Not a valid regular expression pattern.'.")
-                .ValidateDataAnnotations();
+                },
+                "Custom validation failed for members: 'RegexUrlCharacterWhiteList' with the error: 'Not a valid regular expression pattern.'.")
+                .ValidateDataAnnotations()
+                .Validate(o => o.BaseAddress?.IsAbsoluteUri is true, "Must be an absolute URI.");
 
             return services;
         }
